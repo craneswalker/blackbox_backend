06916b4598f59c981a0b42b34a8ba7108da2b8a5
Jacob Crane <jacobcrane.dev@gmail.com>
2018-05-14 15:11:58 -0600

    add cors

--- app.js
+++ app.js
@@ -5,22 +5,9 @@ const morgan = require("morgan")
 const app = express()
 const todo = require("./routes/todoRoutes")
 const done = require("./routes/doneRoutes")
+const cors = require("cors")
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-app.get('/', function(req, res, next) {
-  // Handle the get for this route
-});
-
-app.post('/', function(req, res, next) {
- // Handle the post for this route
-});
-
-
+app.use(cors())
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 
